eb97da78e17445c15941f5a31bba77822627a7fd
gontarev <gontarevsanya@gmail.com>
2022-06-15 18:46:47 +0300

    add faker

--- flask_hw.py
+++ flask_hw.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from faker import Faker
 
 flask_hw = Flask(__name__)
 
@@ -12,6 +13,17 @@ def requierements(filename = "requierements.txt"):
     with open(filename, "r") as txt_file:
         data = txt_file.readlines()
         return '<p></p>'.join(data)
+
+
+@flask_hw.route("/generate_users/")
+def generate_users():
+    fake =Faker()
+    count = 10
+    a=[]
+    for i in range(count):
+        a.append(str(fake.first_name()) + " " + str(fake.ascii_email()))
+
+    return '<p></p>'.join(a)
 
 
 if __name__ == '__main__':
