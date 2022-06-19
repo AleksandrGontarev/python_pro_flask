72fd87e9406446d05a0bd3e479a1d9c74e2bdf61
gontarev <gontarevsanya@gmail.com>
2022-06-19 14:48:27 +0300

    change app to flask_hw

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -2,35 +2,84 @@ import os
 
 from flask import Flask
 
+from flask import Flask, request
+from faker import Faker
+import csv
+import statistics
+import requests
+
 
 def create_app(test_config=None):
     # create and configure the app
-    app = Flask(__name__, instance_relative_config=True)
-    app.config.from_mapping(
+    flask_hw = Flask(__name__, instance_relative_config=True)
+    flask_hw.config.from_mapping(
         SECRET_KEY='dev',
-        DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
+        DATABASE=os.path.join(flask_hw.instance_path, 'flaskr.sqlite'),
     )
 
     if test_config is None:
         # load the instance config, if it exists, when not testing
-        app.config.from_pyfile('config.py', silent=True)
+        flask_hw.config.from_pyfile('config.py', silent=True)
     else:
         # load the test config if passed in
-        app.config.from_mapping(test_config)
+        flask_hw.config.from_mapping(test_config)
 
     # ensure the instance folder exists
     try:
-        os.makedirs(app.instance_path)
+        os.makedirs(flask_hw.instance_path)
     except OSError:
         pass
 
     # a simple page that says hello
-    @app.route('/hello')
+    @flask_hw.route("/")
+    def head():
+        return "Hello !!!"
+
+    @flask_hw.route("/requierements")
+    def requierements(filename="requierements.txt"):
+        result = []
+        with open(filename, "r") as txt_file:
+            data = txt_file.readlines()
+            for i in data:
+                result.append(f'<p>{i}</p>')
+        return " ".join(result)
+
+    @flask_hw.route("/generate_users/")
+    def generate_users():
+        fake = Faker()
+        amount = int(request.args.get('count', 100))
+        names = []
+        result = []
+        for i in range(amount):
+            names.append(str(fake.first_name()) + " " + str(fake.ascii_email()))
+        for i in names:
+            result.append(f'<p>{i}</p>')
+        return ' '.join(result)
+
+    @flask_hw.route("/mean/")
+    def mean():
+        height = []
+        weight = []
+        with open("hw.csv", 'r') as file:
+            reader = csv.DictReader(file, quotechar='"', delimiter=',', quoting=csv.QUOTE_ALL, skipinitialspace=True)
+            for row in reader:
+                height.append(float(row["Height(Inches)"]))
+                weight.append(float(row["Weight(Pounds)"]))
+        mean_h = statistics.mean(height) * 2.54
+        mean_w = statistics.mean(weight) * 0.453592
+        return "<p>Mean height = {} cm</p><p>Mean weight = {} kg</p>".format(mean_h, mean_w)
+
+    @flask_hw.route("/space/")
+    def space():
+        r = requests.get('http://api.open-notify.org/astros.json')
+        result = r.json()['number']
+        return f"{result}"
+
+    @flask_hw.route('/hello')
     def hello():
         return 'Hello, World!'
 
     from . import db
-    db.init_app(app)
+    db.init_app(flask_hw)
 
-
-    return app
+    return flask_hw
--- flaskr/db.py
+++ flaskr/db.py
@@ -37,6 +37,6 @@ def init_db_command():
     init_db()
     click.echo('Initialized the database.')
 
-def init_app(app):
-    app.teardown_appcontext(close_db)
-    app.cli.add_command(init_db_command)
+def init_app(flask_hw):
+    flask_hw.teardown_appcontext(close_db)
+    flask_hw.cli.add_command(init_db_command)
